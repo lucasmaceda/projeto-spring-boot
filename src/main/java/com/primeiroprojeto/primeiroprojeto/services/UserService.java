4f922f8331dc7e6831f2a7ab2e62774e7d2d613f
Lucas Maceda Miranda <lucasmacedamiranda@gmail.com>
2023-01-19 16:07:29 -0300

    .

--- src/main/java/com/primeiroprojeto/primeiroprojeto/services/UserService.java
+++ src/main/java/com/primeiroprojeto/primeiroprojeto/services/UserService.java
@@ -2,8 +2,6 @@ package com.primeiroprojeto.primeiroprojeto.services;
 
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
